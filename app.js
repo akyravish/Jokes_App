2c3ac5e9e9f10a56954ab0ac4cd64609132e6206
akyravish <info.akshayravish@gmail.com>
2021-03-20 06:33:04 +0530

    adding codes for jokes app

---
+++ app.js
@@ -0,0 +1,30 @@
+// jshint esversion:6
+
+const getJokes = (e) => {
+	const numberOfJokes = document.querySelector('#number').value;
+
+	const xhr = new XMLHttpRequest();
+
+	xhr.open('GET', `http://api.icndb.com/jokes/random/${numberOfJokes}`, true);
+
+	xhr.onload = () => {
+		if (xhr.status === 200) {
+			const output = JSON.parse(xhr.responseText);
+			let jokesArray = '';
+			if (output.type === 'success') {
+				output.value.forEach((number) => {
+					jokesArray += `<li>${number.joke}</li>`;
+				});
+			} else {
+				jokesArray += '<li>Something went wrong</li>';
+			}
+			document.querySelector('.jokes').innerHTML = jokesArray;
+		}
+	};
+
+	xhr.send();
+
+	e.preventDefault();
+};
+
+document.querySelector('.get-jokes').addEventListener('click', getJokes);
